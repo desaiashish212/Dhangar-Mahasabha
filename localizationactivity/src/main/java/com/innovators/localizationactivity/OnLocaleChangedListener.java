87d0278b92c6f31e9500fd6dc26927f82d5f98e6
Ashish Desai <desaiashish212@gmail.com>
2016-05-13 16:25:31 +0530

    Commit Message

---
+++ localizationactivity/src/main/java/com/innovators/localizationactivity/OnLocaleChangedListener.java
@@ -0,0 +1,9 @@
+package com.innovators.localizationactivity;
+
+/**
+ * Created by AD on 17-Jan-16.
+ */
+interface OnLocaleChangedListener {
+    void onBeforeLocaleChanged();
+    void onAfterLocaleChanged();
+}
